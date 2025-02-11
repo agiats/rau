32ff083fcb735a5c227103afcfd45aa2b8160800
agiats <taiga98-0809@g.ecc.u-tokyo.ac.jp>
2025-02-11 07:48:32 +0100

    ngram estimatino metho

--- experiments/analysis/collect_result.bash
+++ experiments/analysis/collect_result.bash
@@ -3,17 +3,18 @@ set -euo pipefail
 
 # data_name="PFSA"
 # exp_names=("local_entropy_disjoint" "local_entropy_non_disjoint")
-data_name="BLLIP_SM"
+data_name="babylm2024_100K_sents"
 exp_names=("deterministic_shuffles")
 
 exp_base_dir="$DATA_DIR"/"$data_name"
 split_names=("validation" "test")
+ngram_method="kenlm"
 for split_name in "${split_names[@]}"; do
     for exp_name in "${exp_names[@]}"; do
-        OUTPUT_PATH="${RESULTS_DIR}"/"$data_name"/"$exp_name"/collected_results_"$split_name".csv
+        OUTPUT_PATH="${RESULTS_DIR}"/"$data_name"/"$exp_name"/collected_results_"$ngram_method"_"$split_name".csv
 
         submit_job \
-            collect_result+"$data_name"+"$exp_name"+"$split_name" \
+            collect_result+"$data_name"+"$exp_name"+"$ngram_method"+"$split_name" \
             cpu \
             --time=4:00:00 \
             -- \
@@ -24,6 +25,6 @@ for split_name in "${split_names[@]}"; do
             --architectures "${ARCHITECTURES[@]}" \
             --split_name "$split_name" \
             --output_path "$OUTPUT_PATH" \
-            --metadata_filename "metadata.json"
+            --metadata_filename "metadata_"$ngram_method".json"
     done
 done
--- experiments/analysis/plot_results_natural_language.bash
+++ experiments/analysis/plot_results_natural_language.bash
@@ -2,17 +2,18 @@ set -euo pipefail
 . experiments/include.bash
 
 
-data_name="babylm2024_100K"
+data_name="babylm2024_10M_fixed"
 exp_names=("deterministic_shuffles")
 exp_base_dir="$DATA_DIR"/"$data_name"
 split_names=("validation" "test")
+ngram_method="kenlm"
 for split_name in "${split_names[@]}"; do
     for exp_name in "${exp_names[@]}"; do
-        RESULTS_PATH="${RESULTS_DIR}"/"$data_name"/"$exp_name"/collected_results_"$split_name".csv
-        PLOTS_DIR="${RESULTS_DIR}"/"$data_name"/"$exp_name"/plots
+        RESULTS_PATH="${RESULTS_DIR}"/"$data_name"/"$exp_name"/collected_results_"$ngram_method"_"$split_name".csv
+        PLOTS_DIR="${RESULTS_DIR}"/"$data_name"/"$exp_name"/plots_"$ngram_method"
 
         submit_job \
-        plot_results_natural_language+"$data_name"+"$exp_name"+"$split_name" \
+        plot_results_natural_language+"$data_name"+"$exp_name"+"$ngram_method"+"$split_name" \
         cpu \
         --time=4:00:00 \
         -- \
