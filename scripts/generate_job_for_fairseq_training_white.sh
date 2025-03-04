caa72e4db37dc224e7b0c02252556f4308c04802
agiats <taiga98-0809@g.ecc.u-tokyo.ac.jp>
2025-03-04 09:44:42 +0100

    delte unnecessry file

--- scripts/generate_job_for_fairseq_training_white.sh
+++
@@ -1,4 +0,0 @@
-python scripts/generate_job_for_fairseq_training_white.py \
-    --grammar_names "Base" "DeterministicShuffle" "NonDeterministicShuffle" "LocalShuffle" "EvenOddShuffle" "NoReverse" "PartialReverse" "FullReverse" \
-    --num_splits 10 \
-    --output_dir scripts/jobs/lm_training
