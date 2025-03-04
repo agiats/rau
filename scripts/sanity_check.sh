98b74230f6959f7a7345195c8ab798b6ea20cbd7
agiats <taiga98-0809@g.ecc.u-tokyo.ac.jp>
2025-03-04 09:45:12 +0100

    delete

--- scripts/sanity_check.sh
+++
@@ -1,9 +0,0 @@
-n_samples=100_000_000
-exp_dir="results/length_sampling/100M_samples_eos_zipf_min1_max20/true_probs_count_balanced_samples100"
-
-
-export PYTHONPATH="."
-python scripts/sanity_check.py \
-    --exp_dir $exp_dir \
-    --input_suffix ".csv.gz" \
-    --sample_size $n_samples
