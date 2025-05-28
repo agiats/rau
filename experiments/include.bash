a71916bc1bf4efcd8674160d8c2a0a1d2e5e73ca
agiats <taiga98-0809@g.ecc.u-tokyo.ac.jp>
2025-05-28 14:49:42 +0200

    integrate pfsa scripts

--- experiments/include.bash
+++ experiments/include.bash
@@ -5,8 +5,14 @@ PERTURBATION_CONFIG_FILE=config/perturbation_func.json
 ARCHITECTURES=("lstm" "transformer" "stack-rnn_lstm_superposition-20" "stack-transformer_32-2.superposition-32.2" "stack-rnn_lstm_512_superposition-64" "stack-transformer_768-2.superposition-64.2")
 ARCHITECTURE_LABELS=("LSTM" "Transformer" "RNN+Sup. 20" "Tf+Sup. 32" "RNN+Sup. 64" "Tf+Sup. 64")
 
-
 NUM_TRIALS=5
+
+# PFSA dataset generation parameters
+N_SYMBOLS_LIST=(32 48 64)
+N_STATES_LIST=(16 24 32)
+N_TOPOLOGY_SEEDS=5
+N_WEIGHT_SEEDS=5
+
 submit_job() {
   bash experiments/submit_job.bash "$@"
 }
