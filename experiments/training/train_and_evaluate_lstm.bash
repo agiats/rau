c54c2f6bfc46686c229b64e439f6ada4fe163ac6
agiats <taiga98-0809@g.ecc.u-tokyo.ac.jp>
2025-05-28 13:53:17 +0200

    cleanup

--- experiments/training/train_and_evaluate_lstm.bash
+++ experiments/training/train_and_evaluate_lstm.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=submit_jobs_lstm
-#SBATCH --output=/cluster/home/tsomeya/projects/lm_inductive_bias/logs/submit_jobs_lstm.out
-#SBATCH --error=/cluster/home/tsomeya/projects/lm_inductive_bias/logs/submit_jobs_lstm.err
+#SBATCH --output=logs/submit_jobs_lstm.out
+#SBATCH --error=logs/submit_jobs_lstm.err
 #SBATCH --time=04:00:00  # Adjust time as needed
 #SBATCH --mem-per-cpu=4G         # Adjust memory as needed
 #SBATCH --tmp=20g
--- experiments/training/train_and_evaluate_transformer.bash
+++ experiments/training/train_and_evaluate_transformer.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=submit_jobs_transformer
-#SBATCH --output=/cluster/home/tsomeya/projects/lm_inductive_bias/logs/submit_jobs_transformer.out
-#SBATCH --error=/cluster/home/tsomeya/projects/lm_inductive_bias/logs/submit_jobs_transformer.err
+#SBATCH --output=logs/submit_jobs_transformer.out
+#SBATCH --error=logs/submit_jobs_transformer.err
 #SBATCH --time=04:00:00  # Adjust time as needed
 #SBATCH --mem-per-cpu=4G         # Adjust memory as needed
 #SBATCH --tmp=20g
